36a4e622cb7c773d6185d04b8721ce7e03f79189
Azazell <Azazellfn@gmail.com>
2019-05-28 12:44:06 +0300

    Fixed ME m.c. gui wasn't opening

--- src/main/java/AppliedIntegrations/Gui/MultiController/GuiMultiControllerCore.java
+++ src/main/java/AppliedIntegrations/Gui/MultiController/GuiMultiControllerCore.java
@@ -4,9 +4,6 @@ package AppliedIntegrations.Gui.MultiController;
 import AppliedIntegrations.Container.tile.MultiController.ContainerMultiControllerCore;
 import AppliedIntegrations.Gui.AIBaseGui;
 import AppliedIntegrations.api.ISyncHost;
-import AppliedIntegrations.api.Multiblocks.BlockData;
-import appeng.client.gui.AEBaseGui;
-import appeng.client.gui.widgets.GuiScrollbar;
 import appeng.core.AppEng;
 import appeng.core.localization.GuiText;
 import net.minecraft.client.Minecraft;
@@ -19,18 +16,9 @@ import org.lwjgl.opengl.GL11;
  */
 public class GuiMultiControllerCore extends AIBaseGui {
 	private static final ResourceLocation texture = new ResourceLocation(AppEng.MOD_ID, "textures/guis/terminal.png");
-	private GuiScrollbar bar;
 
 	public GuiMultiControllerCore(ContainerMultiControllerCore container, EntityPlayer p) {
 		super(container, p);
-	}
-
-	@Override
-	public void initGui() {
-		super.initGui();
-
-		// Init scroll bar
-		bar = new GuiScrollbar();
 	}
 
 	@Override
@@ -45,7 +33,7 @@ public class GuiMultiControllerCore extends AIBaseGui {
 		Minecraft.getMinecraft().renderEngine.bindTexture(texture);
 
 		// Draw texture
-		drawTexturedModalRect(this.guiLeft, this.guiTop - 15, 0, 0, 190, 200);
+		drawTexturedModalRect(this.guiLeft, this.guiTop - 15, 0, 0, 195, 200);
 	}
 
 	@Override
@@ -55,30 +43,6 @@ public class GuiMultiControllerCore extends AIBaseGui {
 		// Draw string
 		this.fontRenderer.drawString(GuiText.inventory.getLocal(), 7, this.ySize - 108, 4210752); // (Player inv.)
 		this.fontRenderer.drawString("ME Network Card Drive", 7, -12, 4210752); // (Server drive inv)
-
-		// Draw bar
-		bar.draw(new AEBaseGui(this.inventorySlots) {
-			@Override
-			public void drawTexturedModalRect(int x, int y, int textureX, int textureY, int width, int height) {
-				// Pass call to given GUI
-				GuiMultiControllerCore.this.drawTexturedModalRect(x, y, textureX, textureY, width, height);
-			}
-
-			@Override
-			public void drawFG(int offsetX, int offsetY, int mouseX, int mouseY) {
-				// Ignored
-			}
-
-			@Override
-			public void drawBG(int offsetX, int offsetY, int mouseX, int mouseY) {
-				// Ignored
-			}
-
-			@Override
-			protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-				// Ignored
-			}
-		});
 	}
 
 
